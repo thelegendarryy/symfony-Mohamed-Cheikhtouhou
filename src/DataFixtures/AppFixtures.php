629fcdb690d632f1b33ee8882fb358b2262a1312
mohamed cheikhrouhou <medchie420@gmail.com>
2023-12-04 14:29:42 +0100

    cheikhrouhou

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\VinylMix;
+use App\Repository\VinylMixFactory;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        VinylMixFactory::createMany(25);
+
+        $manager->flush();
+    }
+}
